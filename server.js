f9af54eb8d1f2da5f5cc07e7ae9ab65756d1507e
RachelY <yang.yijing@outlook.com>
2024-02-17 11:01:28 -0500

    controllers added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require('cors')
+const bookmarkController = require('./controllers/bookmark')
+const bodyParser = require('body-parser');
 
 
 const app = express()
@@ -10,7 +12,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())// allows us to send a JSON request with our request (like with POST request)
-
+app.use('/bookmark', bookmarkController)
 
 
 // routes
